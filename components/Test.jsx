843e6eee7aface579da47eb96176bba5067b501a
hurlx <n247684@gmail.com>
2025-07-17 21:32:32 +0300

    Update Test.jsx

--- components/Test.jsx
+++ components/Test.jsx
@@ -1,14 +1,16 @@
+import { abilities } from "@/constants";
+import GlowCard from "./GlowCard";
 
 const Test = () => {
   return (
     <div
 			dir="rtl"
-			className="min-h-screen bg-[#faebf2] px-6 py-16 text-gray-800"
+			className="min-h-screen px-6 py-16 text-gray-800 relative z-10"
 		>
 			{/* Hero Section */}
 			<section className="text-center mb-20">
 				<h1 className="text-5xl font-bold mb-4">من نحن</h1>
-				<p className="text-lg text-gray-600 max-w-2xl mx-auto">
+				<p className="text-lg text-gray-500 max-w-2xl mx-auto">
 					وجهتك المميزة للحصول على حقائب عالية الجودة، مصممة بشغف ومقدمة بعناية.
 				</p>
 			</section>
@@ -16,7 +18,7 @@ const Test = () => {
 			{/* Our Story */}
 			<section className="max-w-4xl mx-auto mb-16">
 				<h2 className="text-3xl font-semibold mb-4">قصتنا</h2>
-				<p className="text-gray-700 leading-relaxed">
+				<p className="text-gray-500 leading-relaxed">
 					بدأنا برؤية بسيطة — تقديم حقائب أنيقة، متينة وعملية للجميع. من متجر
 					صغير، تطورنا إلى علامة تجارية موثوقة معروفة بالجودة والاهتمام
 					بالعملاء.
@@ -26,7 +28,7 @@ const Test = () => {
 			{/* Services */}
 			<section className="max-w-4xl mx-auto mb-16">
 				<h2 className="text-3xl font-semibold mb-4">خدماتنا</h2>
-				<ul className="list-disc list-inside text-gray-700 space-y-2">
+				<ul className="list-disc list-inside text-gray-500 space-y-2">
 					<li>شحن سريع وموثوق لجميع أنحاء المنطقة</li>
 					<li>دعم عملاء عالي الجودة على مدار الساعة</li>
 					<li>سهولة في الاستبدال والإرجاع</li>
@@ -48,6 +50,18 @@ const Test = () => {
 					</div>
 				</div>
 			</section>
+			<div className="lg:columns-3 md:columns-2 columns-1 mt-16">
+          {abilities.map((abilities, index) => (
+            <GlowCard card={abilities} key={index} index={index}>
+              <div className="flex items-center gap-3">
+                <div>
+                  <p className="font-bold">{abilities.title}</p>
+                  <p className="text-white-50">{abilities.desc}</p>
+                </div>
+              </div>
+            </GlowCard>
+          ))}
+        </div>
 		</div>
   );
 };
